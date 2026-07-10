57562ec20611db634ea1445bac1b4b4c35464de8
Jari1980 <jari.tenhunen@outlook.com>
2026-07-10 18:54:54 +0200

    first commit

---
+++ backend/src/main/java/ESP32/Emulator/Esp32EmulatorApplication.java
@@ -0,0 +1,13 @@
+package ESP32.Emulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esp32EmulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esp32EmulatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ESP32/Emulator/Esp32EmulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ESP32.Emulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esp32EmulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
