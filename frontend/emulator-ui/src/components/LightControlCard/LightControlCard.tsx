345d80833842641b3ffb974db0d048e8c5a02992
Jari1980 <jari.tenhunen@outlook.com>
2026-07-13 14:28:56 +0200

    send led commands over websocket

--- frontend/emulator-ui/src/components/LightControlCard/LightControlCard.tsx
+++ frontend/emulator-ui/src/components/LightControlCard/LightControlCard.tsx
@@ -1,11 +1,21 @@
 import Card from "../Card/Card";
 import { useEsp32 } from "../../context/Esp32Context";
+import useDeviceCommand from "../../hooks/useDeviceCommand";
+
 import "./LightControlCard.css";
 
 function LightControlCard() {
   const { state } = useEsp32();
+  const { sendCommand } = useDeviceCommand();
   const led = state.devices.find((device) => device.deviceId === "led-001");
   const ledOn = led?.state.ledOn === true;
+  const handleToggle = () => {
+    sendCommand(
+      ledOn ? "TURN_OFF" : "TURN_ON",
+
+      "led-001",
+    );
+  };
 
   return (
     <Card title="Light / LED">
@@ -20,7 +30,7 @@ function LightControlCard() {
           {ledOn ? "ON" : "OFF"}
         </div>
 
-        <button className="light-control__button" disabled>
+        <button className="light-control__button" onClick={handleToggle}>
           Toggle Light
         </button>
       </div>
