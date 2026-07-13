afb6ebafd223f7dd9ee7b1caae02fa379f08d9a4
Jari1980 <jari.tenhunen@outlook.com>
2026-07-13 12:18:36 +0200

    temperature events published

--- frontend/emulator-ui/src/components/TemperatureControlCard/TemperatureControlCard.tsx
+++ frontend/emulator-ui/src/components/TemperatureControlCard/TemperatureControlCard.tsx
@@ -1,18 +1,32 @@
 import Card from "../Card/Card";
 import "./TemperatureControlCard.css";
 import useTemperature from "../../hooks/useTemperature";
+import { useEvents } from "../../context/EventContext";
 
 function TemperatureControlCard() {
+  const { addEvent } = useEvents();
   const { temperature, increase, decrease } = useTemperature();
+
+  const increaseTemperature = () => {
+    increase();
+
+    addEvent("Temperature increased");
+  };
+
+  const decreaseTemperature = () => {
+    decrease();
+
+    addEvent("Temperature decreased");
+  };
   return (
     <Card title="Temperature Sensor">
       <div className="temperature-control">
         <div className="temperature-control__value">{temperature} °C</div>
 
         <div className="temperature-control__buttons">
-          <button onClick={decrease}>-</button>
+          <button onClick={decreaseTemperature}>-</button>
 
-          <button onClick={increase}>+</button>
+          <button onClick={increaseTemperature}>+</button>
         </div>
       </div>
     </Card>
