693d4ff4866e1ca076f3507fdbe1b5c5e5ace0aa
Jari1980 <jari.tenhunen@outlook.com>
2026-07-27 12:01:07 +0200

    initial control unit

---
+++ control-unit/src/main/java/controlunit/ControlUnitApplication.java
@@ -0,0 +1,13 @@
+package controlunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlUnitApplication.class, args);
+	}
+
+}
---
+++ control-unit/src/test/java/controlunit/ControlUnitApplicationTests.java
@@ -0,0 +1,13 @@
+package controlunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlUnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
