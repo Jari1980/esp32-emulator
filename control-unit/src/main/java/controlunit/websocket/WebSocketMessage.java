48860823d999616b9112e8340cc561f42773b943
Jari1980 <jari.tenhunen@outlook.com>
2026-07-28 11:09:26 +0200

    websocket message model

---
+++ control-unit/src/main/java/controlunit/websocket/WebSocketMessage.java
@@ -0,0 +1,14 @@
+package controlunit.websocket;
+
+public class WebSocketMessage {
+    private String type;
+    private String payload;
+
+    public String getType() {
+        return type;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+}
